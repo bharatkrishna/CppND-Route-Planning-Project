5643cf04ad2b33fd297d82126dcd2321ebc2d670
Bharat Krishna <bharatkrishna.r@gmail.com>
2019-08-18 21:30:16 -0700

    Progress so far

--- src/route_model.h
+++ src/route_model.h
@@ -12,9 +12,18 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
+        Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+
+        float distance(Node other) const {
+          return std::sqrt(std::pow((x - other.x), 2) + std::pow((y - other.y), 2));
+        };
       
       private:
         // Add private Node variables and methods here.
@@ -25,8 +34,13 @@ class RouteModel : public Model {
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
+    auto &SNodes() { return m_Nodes; }
+    auto& GetNodeToRoadMap() { return node_to_road; }
 
   private:
     // Add private RouteModel variables and methods here.
+    std::vector<Node> m_Nodes;
+    std::unordered_map<int, std::vector<const Model::Road*>> node_to_road;
 
+    void CreateNodeToRoadHashmap() {}
 };
